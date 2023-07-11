7879959f118b695433203677d59164b99fe7396b
ashutosh <jhaashu09868@gmail.com>
2023-07-11 19:10:59 +0530

    server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://mern-go-foood.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
